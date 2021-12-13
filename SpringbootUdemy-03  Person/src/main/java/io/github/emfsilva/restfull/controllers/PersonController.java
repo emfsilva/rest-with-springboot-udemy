78b13f511547ad4ae4e56ecc077e76ea2c9d7e63
Emerson Ferreira <emersonfdasilva2@gmail.com>
2021-12-13 16:30:08 -0300

    Implementando os métodos HTTP

--- SpringbootUdemy-03  Person/src/main/java/io/github/emfsilva/restfull/controllers/PersonController.java
+++ SpringbootUdemy-03  Person/src/main/java/io/github/emfsilva/restfull/controllers/PersonController.java
@@ -4,10 +4,9 @@ import io.github.emfsilva.restfull.model.Person;
 import io.github.emfsilva.restfull.services.PersonService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.MediaType;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestMethod;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
 
 
 @RestController
@@ -21,9 +20,32 @@ public class PersonController {
         this.service = service;
     }
 
-    @RequestMapping(value = "/{id}", method = RequestMethod.GET,
-            produces = MediaType.APPLICATION_JSON_VALUE)
+    @RequestMapping(method = RequestMethod.GET, produces = MediaType.APPLICATION_JSON_VALUE)
+    public List<Person> findAll() {
+        return service.findAll();
+    }
+
+    @RequestMapping(value = "/{id}", method = RequestMethod.GET, produces = MediaType.APPLICATION_JSON_VALUE)
     public Person findById(@PathVariable("id") String id) {
         return service.findById(id);
+    }
+
+    @RequestMapping(method = RequestMethod.POST,
+            produces = MediaType.APPLICATION_JSON_VALUE,
+            consumes = MediaType.APPLICATION_JSON_VALUE)
+    public Person create(@RequestBody Person person) {
+        return service.create(person);
+    }
+
+    @RequestMapping(method = RequestMethod.PUT,
+            produces = MediaType.APPLICATION_JSON_VALUE,
+            consumes = MediaType.APPLICATION_JSON_VALUE)
+    public Person update(@RequestBody Person person) {
+        return service.create(person);
+    }
+
+    @RequestMapping(value = "/{id}",method = RequestMethod.DELETE)
+    public void delete(@PathVariable("id") String id) {
+        service.delete(id);
     }
 }
--- SpringbootUdemy-03  Person/src/main/java/io/github/emfsilva/restfull/services/PersonService.java
+++ SpringbootUdemy-03  Person/src/main/java/io/github/emfsilva/restfull/services/PersonService.java
@@ -3,6 +3,8 @@ package io.github.emfsilva.restfull.services;
 import io.github.emfsilva.restfull.model.Person;
 import org.springframework.stereotype.Service;
 
+import java.util.ArrayList;
+import java.util.List;
 import java.util.concurrent.atomic.AtomicLong;
 
 @Service
@@ -18,5 +20,37 @@ public class PersonService {
         person.setAddress("Sao Paulo - Brazil");
         person.setGender("Male");
         return person;
+    }
+
+    public List<Person> findAll() {
+        List<Person> persons = new ArrayList<>();
+        for (int i = 0; i < 8; i++) {
+            Person person = mockPerson(i);
+            persons.add(person);
+        }
+
+        return persons;
+    }
+
+    private Person mockPerson(int i) {
+        Person person = new Person();
+        person.setId(counter.incrementAndGet());
+        person.setFirstName("Person Name " + i);
+        person.setLastName("Last Name " + i);
+        person.setAddress("Some - Brazil " + i);
+        person.setGender("Male");
+        return person;
+    }
+
+    public Person create(Person person) {
+        return person;
+    }
+
+    public Person update(Person person) {
+        return person;
+    }
+
+    public void delete (String id){
+
     }
 }
