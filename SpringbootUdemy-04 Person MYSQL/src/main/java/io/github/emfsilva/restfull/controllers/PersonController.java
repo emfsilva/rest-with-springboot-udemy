95916eabdba13cf8e362605bb79a2c164444b767
Emerson Ferreira <emersonfdasilva2@gmail.com>
2021-12-13 17:58:22 -0300

    implementação e configuração da base de dados mysql

---
+++ SpringbootUdemy-04 Person MYSQL/src/main/java/io/github/emfsilva/restfull/controllers/PersonController.java
@@ -0,0 +1,51 @@
+package io.github.emfsilva.restfull.controllers;
+
+import io.github.emfsilva.restfull.model.Person;
+import io.github.emfsilva.restfull.services.PersonService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.MediaType;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+
+
+@RestController
+@RequestMapping("/person")
+public class PersonController {
+
+    private PersonService service;
+
+    @Autowired
+    public PersonController(PersonService service) {
+        this.service = service;
+    }
+
+    @RequestMapping(method = RequestMethod.GET, produces = MediaType.APPLICATION_JSON_VALUE)
+    public List<Person> findAll() {
+        return service.findAll();
+    }
+
+    @RequestMapping(value = "/{id}", method = RequestMethod.GET, produces = MediaType.APPLICATION_JSON_VALUE)
+    public Person findById(@PathVariable("id") String id) {
+        return service.findById(id);
+    }
+
+    @RequestMapping(method = RequestMethod.POST,
+            produces = MediaType.APPLICATION_JSON_VALUE,
+            consumes = MediaType.APPLICATION_JSON_VALUE)
+    public Person create(@RequestBody Person person) {
+        return service.create(person);
+    }
+
+    @RequestMapping(method = RequestMethod.PUT,
+            produces = MediaType.APPLICATION_JSON_VALUE,
+            consumes = MediaType.APPLICATION_JSON_VALUE)
+    public Person update(@RequestBody Person person) {
+        return service.create(person);
+    }
+
+    @RequestMapping(value = "/{id}",method = RequestMethod.DELETE)
+    public void delete(@PathVariable("id") String id) {
+        service.delete(id);
+    }
+}
---
+++ SpringbootUdemy-04 Person MYSQL/src/test/java/io/github/emfsilva/restfull/RestfullApplicationTests.java
@@ -0,0 +1,13 @@
+package io.github.emfsilva.restfull;
+
+import org.junit.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RestfullApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
