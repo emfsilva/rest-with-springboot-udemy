57e5ff2f47a9189b588e38a7c4cd11aa1a5e6655
Emerson Ferreira <emersonfdasilva2@gmail.com>
2021-12-13 12:12:20 -0300

    Implementando as demais Operações

---
+++ SpringbootUdemy-02  Calculator/src/main/java/io/github/emfsilva/restfull/controllers/MathController.java
@@ -0,0 +1,85 @@
+package io.github.emfsilva.restfull.controllers;
+
+import io.github.emfsilva.restfull.converters.NumberConverter;
+import io.github.emfsilva.restfull.math.SimpleMath;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class MathController {
+
+    private NumberConverter numberConverter;
+    SimpleMath math = new SimpleMath();
+
+    @Autowired
+    public MathController(NumberConverter numberConverter) {
+        this.numberConverter = numberConverter;
+    }
+
+
+    @RequestMapping(value = "/sum/{numberOne}/{numberTwo}", method = RequestMethod.GET)
+    public Double sum(@PathVariable("numberOne") String numberOne, @PathVariable("numberTwo") String numberTwo) {
+        if (!numberConverter.isNumeric(numberOne) || !numberConverter.isNumeric(numberTwo)) {
+            throw new UnsupportedOperationException("Please set a numeric value!");
+        }
+
+        return math.sum(numberConverter.convertToDouble(numberOne), numberConverter.convertToDouble(numberTwo));
+    }
+
+    @RequestMapping(value = "/subtraction/{numberOne}/{numberTwo}", method = RequestMethod.GET)
+    public Double subtraction(@PathVariable("numberOne") String numberOne, @PathVariable("numberTwo") String numberTwo) {
+        if (!numberConverter.isNumeric(numberOne) || !numberConverter.isNumeric(numberTwo)) {
+            throw new UnsupportedOperationException("Please set a numeric value!");
+        }
+
+        return math.subtraction(numberConverter.convertToDouble(numberOne), numberConverter.convertToDouble(numberTwo));
+    }
+
+    @RequestMapping(value = "/multiplication/{numberOne}/{numberTwo}", method = RequestMethod.GET)
+    public Double multiplication(@PathVariable("numberOne") String numberOne, @PathVariable("numberTwo") String numberTwo) {
+        if (!numberConverter.isNumeric(numberOne) || !numberConverter.isNumeric(numberTwo)) {
+            throw new UnsupportedOperationException("Please set a numeric value!");
+        }
+
+        return math.multiplication(numberConverter.convertToDouble(numberOne), numberConverter.convertToDouble(numberTwo));
+    }
+
+    @RequestMapping(value = "/division/{numberOne}/{numberTwo}", method = RequestMethod.GET)
+    public Double division(@PathVariable("numberOne") String numberOne, @PathVariable("numberTwo") String numberTwo) {
+        if (!numberConverter.isNumeric(numberOne) || !numberConverter.isNumeric(numberTwo)) {
+            throw new UnsupportedOperationException("Please set a numeric value!");
+        }
+
+        return math.division(numberConverter.convertToDouble(numberOne), numberConverter.convertToDouble(numberTwo));
+    }
+
+    @RequestMapping(value = "/mean/{numberOne}/{numberTwo}", method = RequestMethod.GET)
+    public Double mean(@PathVariable("numberOne") String numberOne, @PathVariable("numberTwo") String numberTwo) {
+        if (!numberConverter.isNumeric(numberOne) || !numberConverter.isNumeric(numberTwo)) {
+            throw new UnsupportedOperationException("Please set a numeric value!");
+        }
+
+        return math.mean(numberConverter.convertToDouble(numberOne), numberConverter.convertToDouble(numberTwo));
+    }
+
+    @RequestMapping(value = "/square/{number}", method = RequestMethod.GET)
+    public Double square(@PathVariable("number") String number) {
+        if (!numberConverter.isNumeric(number)) {
+            throw new UnsupportedOperationException("Please set a numeric value!");
+        }
+        return math.squareRoot(numberConverter.convertToDouble(number));
+
+    }
+
+    @RequestMapping(value = "/remnant/{numberOne}/{numberTwo}", method = RequestMethod.GET)
+    public String remnant(@PathVariable("numberOne") String numberOne, @PathVariable("numberTwo") String numberTwo) {
+        if (!numberConverter.isNumeric(numberOne) || !numberConverter.isNumeric(numberTwo)) {
+            throw new UnsupportedOperationException("Please set a numeric value!");
+        }
+
+        return math.remnant(numberConverter.convertToDouble(numberOne), numberConverter.convertToDouble(numberTwo));
+    }
+}
