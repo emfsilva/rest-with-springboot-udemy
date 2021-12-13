a7721c7141882c8c236d1adf8e51723063a1da14
Emerson Ferreira <emersonfdasilva2@gmail.com>
2021-12-13 18:25:25 -0300

    Implementando Entidade e Repositorio

--- SpringbootUdemy-04 Person MYSQL/src/main/java/io/github/emfsilva/restfull/model/Person.java
+++ SpringbootUdemy-04 Person MYSQL/src/main/java/io/github/emfsilva/restfull/model/Person.java
@@ -5,18 +5,32 @@ import lombok.Getter;
 import lombok.NoArgsConstructor;
 import lombok.Setter;
 
+import javax.persistence.*;
 import java.io.Serializable;
 
 @NoArgsConstructor
 @Getter
 @Setter
 @EqualsAndHashCode
+
+@Entity
+@Table(name = "person")
 public class Person implements Serializable {
     private static final long serialVersionUID = -2157592387257060508L;
 
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
+
+    @Column(name = "first_name", nullable = false, length = 80)
     private String firstName;
+
+    @Column(name = "last_name", nullable = false,length = 80)
     private String lastName;
+
+    @Column(nullable = false, length = 100)
     private String address;
+
+    @Column(nullable = false, length = 6)
     private String gender;
 }
