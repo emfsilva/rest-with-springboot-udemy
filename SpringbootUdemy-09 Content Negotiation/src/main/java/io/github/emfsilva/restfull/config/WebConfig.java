eb2d8af1a96d9772293f9bb3ce8368005925e9d5
Emerson Ferreira <emersonfdasilva2@gmail.com>
2021-12-15 13:16:37 -0300

    Content Negociation via query parameter

--- SpringbootUdemy-09 Content Negotiation/src/main/java/io/github/emfsilva/restfull/config/WebConfig.java
+++ SpringbootUdemy-09 Content Negotiation/src/main/java/io/github/emfsilva/restfull/config/WebConfig.java
@@ -1,2 +1,36 @@
-package io.github.emfsilva.restfull.config;public class WebConfig {
+package io.github.emfsilva.restfull.config;
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.http.MediaType;
+import org.springframework.web.servlet.config.annotation.ContentNegotiationConfigurer;
+import org.springframework.web.servlet.config.annotation.EnableWebMvc;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+
+@Configuration
+@EnableWebMvc
+public class WebConfig implements WebMvcConfigurer {
+
+    @Override
+    public void configureContentNegotiation(ContentNegotiationConfigurer configurer) {
+
+        // http://localhost:1001/api/person/v1.xml
+/*        configurer
+                .favorParameter(false)
+                .ignoreAcceptHeader(false)
+                .defaultContentType(MediaType.APPLICATION_JSON)
+                .mediaType("json", MediaType.APPLICATION_JSON)
+                .mediaType("xml", MediaType.APPLICATION_XML);*/
+
+
+        // http://localhost:1001/api/person/v1?mediaType=json
+        configurer
+                .favorPathExtension(false)
+                .favorParameter(true)
+                .parameterName("mediaType")
+                .ignoreAcceptHeader(true)
+                .useRegisteredExtensionsOnly(false)
+                .defaultContentType(MediaType.APPLICATION_JSON)
+                .mediaType("json", MediaType.APPLICATION_JSON)
+                .mediaType("xml", MediaType.APPLICATION_XML);
+    }
 }
